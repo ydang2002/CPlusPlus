95515be2ef17abb58d0000cd295898c36d512e53
ydang2002 <nhuy.danghuynh2002@gmail.com>
2021-04-04 08:13:14 +0700

    xoa doan cuoi

--- BaiKiem Tra 1/bai2.1kt1/bai2.1kt1/bai2.1kt1/bai2.1kt1.cpp
+++ BaiKiem Tra 1/bai2.1kt1/bai2.1kt1/bai2.1kt1/bai2.1kt1.cpp
@@ -52,6 +52,7 @@ void Recursive(int a[], int n, int& max1, int& max2)
 int main()
 {
     int n;
+    cin >> n;
     int a[100];
     int max1, max2;
     max1 = max2 = a[0];
@@ -59,13 +60,4 @@ int main()
     Recursive(a, n, max1, max2);
     cout << max2;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
