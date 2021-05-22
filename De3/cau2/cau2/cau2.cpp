deeac93dce2a312637fe3eb6c88be0d17005c555
LAPTOP-38ED4DO3\ASUS <nhuy.danghuynh2002@gmail.com>
2021-05-22 18:33:10 +0700

    de3

--- De3/cau2/cau2/cau2.cpp
+++ De3/cau2/cau2/cau2.cpp
@@ -1,7 +1,15 @@
 ﻿/*Văn phòng Khoa ghi nhận danh sách sinh viên. Họ và tên sinh viên được đặt trên mỗi dòng trong
 một tập tin có tên “danhsach.inp”, đầu và cuối họ tên không có khoảng trắng, ở giữa mỗi từ có một
 khoảng trắng. Hãy viết chương trình thực hiện:Sắp xếp danh sách sinh viên theo thứ tự tăng dần của
-phần tên sinh viên. Danh sách được in lên tập tin có tên “DS_Sort.out”.*/
+phần tên sinh viên. Danh sách được in lên tập tin có tên “DS_Sort.out”.
+strcmp dùng để so sánh trong chuoi
+if (strcmp(str1, str2) == 0) {
+   printf("2 chuoi bang nhau.");
+  } else if (strcmp(str1, str2) > 0) {
+   printf("Chuoi 1 lon hon chuoi 2");
+  } else {
+   printf("Chuoi 1 nho hon chuoi 2");
+  }*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string.h>
@@ -54,11 +62,13 @@ void Sapxep(list sv[], int n)
         }
     }
 }
-void Xuat(list sv[], int n)
+void GhiDSSinhVien(FILE* F, list sv[], int n)
 {
     for (int i = 0; i < n; i++)
     {
         cout << sv[i].s << " \n";
+        fputs(sv[i].s, F);
+        fputs("\n", F);
     }
 }
 
@@ -67,7 +77,9 @@ int main()
     int n;
     list sv[200];
     DocDanhSachSV(sv, n);
-    Sapxep(sv, n);
-    Xuat(sv, n);
+    Sapxep( sv, n);
+    FILE* F = fopen("DS_Sort.out", "w");
+    GhiDSSinhVien(F,sv, n);
+    fclose(F);
 }
 
